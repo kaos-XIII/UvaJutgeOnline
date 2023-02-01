39a33671b8ec4f6b7cea480e33d84af9673271e1
Alex Veiga <kaos1323@gmail.com>
2023-02-01 14:43:32 +0100

    Refactorizar codigo

--- 100_3nplus1/p100.c
+++ 100_3nplus1/p100.c
@@ -1,4 +1,4 @@
- #include <cstdio>
+#include <cstdio>
 #include <algorithm>
 #define long long LL
  
@@ -38,8 +38,8 @@ int main () {
 
     int i, j;
  
-    while ( scanf ("%d %d", &i, &j) != EOF ) {
- 
+    while ( scanf ("%d %d", &i, &j) != EOF ) 
+    {
         int temp_i = i;
         int temp_j = j;
  
@@ -48,28 +48,24 @@ int main () {
         int max_cycle_length = 0;
         int cycle_length;
  
-        while ( i <= j ) {
-
+        while ( i <= j ) 
+        {
             unsigned int n = i;
             cycle_length = 1;
  
-            while ( n != 1 ) {
-            
+            while ( n != 1 ) 
+            {
                 if ( n % 2 == 1 ) n = 3 * n + 1;
                 else n /= 2;
             
                 cycle_length++;
-            
             }
  
             if ( cycle_length > max_cycle_length ) max_cycle_length = cycle_length;
  
             i++;
-
         }
- 
         printf ("%d %d %d\n", temp_i, temp_j, max_cycle_length);
     }
- 
     return 0;
 }
