1f5e3d057b31a4d828a728843134132cb7c1f07c
Alex Veiga <kaos1323@gmail.com>
2023-02-03 10:10:26 +0100

    Añadida estructura del p106

---
+++ 106_FermatVsPythagoras/README.md
@@ -0,0 +1,9 @@
+# UvaJutgeOnline
+
+## Problem 106 - Fermat vs. Pythagoras
+
+## My Times
+
+| Verdict | Lang | Time | Best | Rank | Submit Time |
+| --- | --- | --- | --- | --- | --- |
+| Accepted | C++ | X | 0.008 | X | X |
---
+++ 106_FermatVsPythagoras/p106.c
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main() 
+{
+	int long N = 0;
+
+	while ( scanf ("%i %i %i", &N) != EOF ) 
+	{
+
+	}
+	
+	//// Print
+
+  	return(0);
+}
