793c568c40d126646b907b83105201bf3830b987
Alex Veiga <kaos1323@gmail.com>
2023-02-07 20:17:30 +0100

    Añadido el p107 y modificacion del gitignore

---
+++ 107_TheCatInTheHat/p107.c
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <algorithm>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+#define DEBUG
+
+int main() 
+{
+	unsigned long N = 0;
+
+	while ( scanf ("%i", &N) != EOF ) 
+	{
+		
+		//// Print
+		cout << nTripletaGood << " " << p << "\n";
+	}
+  	return(0);
+}
