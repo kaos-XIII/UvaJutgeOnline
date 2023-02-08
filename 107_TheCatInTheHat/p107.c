ff5bb17c38aa6de4215aaea6cc12ad80b5615e10
Alex Veiga <kaos1323@gmail.com>
2023-02-08 09:38:12 +0100

    p107 - Leve modificacion de codigo

--- 107_TheCatInTheHat/p107.c
+++ 107_TheCatInTheHat/p107.c
@@ -14,13 +14,17 @@ using namespace std;
 
 int main() 
 {
-	unsigned long N = 0;
+	unsigned long height = 0;
+	unsigned long nCats = 0;
+	unsigned long catsNotWorking = 0;
+	unsigned long stackOfCats = 0;
 
-	while ( scanf ("%i", &N) != EOF ) 
+	while ( scanf ("%i %i", &height, &nCats) != EOF ) 
 	{
-		
+		if((height == 0) && (nCats == 0)) break;
+
 		//// Print
-		cout << nTripletaGood << " " << p << "\n";
+		cout << catsNotWorking << " " << stackOfCats << "\n";
 	}
   	return(0);
 }
